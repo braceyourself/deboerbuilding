9c22c6c648604bc1b0490994ed32470b2c057524
braceyourself <ethanabrace@gmail.com>
2025-02-10 17:07:29 -0500

    animations plz

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
 import 'flowbite/dist/flowbite.min.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 import.meta.glob([
     '../images/**',
     // '../fonts/**',
 ]);
+
+AOS.init({
+    duration: 800,
+    easing: 'slide',
+    once: true,
+    delay: 200,
+});
--- resources/views/livewire/about-page/index.blade.php
+++ resources/views/livewire/about-page/index.blade.php
@@ -11,58 +11,58 @@
     @foreach($this->data as $k => $item)
         @php($flip = $k % 2 == 0)
 
-        <div class="lg:py-24" x-data="{hovering: false}" >
-            <div class="bg-gray-900" x-on:mouseover="hovering = true" x-on:mouseleave="hovering = false">
-                <div @class([
-                    "flex flex-col justify-center lg:gap-8",
-                    'py-24 sm:py-32 lg:py-0',
-                    'px-4 lg:px-8',
-                    'lg:flex-row' => $flip,
-                    'lg:flex-row-reverse' => !$flip,
-                ])>
 
-                    <div class="lg:-my-8 shadow lg:min-w-[300px] flex-grow">
-                        <div class="h-full w-full flex justify-center shadow rounded-xl lg:border border-primary" >
-                            <img src="/storage/{{ $item['image'] }}"
-                                 class="object-cover shadow-none lg:shadow-xl rounded-xl
+        <div @class([
+            "lg:py-24",
+            "flex flex-col justify-center lg:gap-8",
+            'py-24 sm:py-32 lg:py-0',
+            'px-4 lg:px-8',
+            'lg:flex-row' => $flip,
+            'lg:flex-row-reverse' => !$flip,
+        ])>
+
+            <div class="lg:-my-8 shadow lg:min-w-[300px] flex-grow"
+                 data-aos="{{ $flip ? 'fade-right' : 'fade-left' }}"
+            >
+                <div class="h-full w-full flex justify-center shadow rounded-xl lg:border border-primary" >
+                    <img src="/storage/{{ $item['image'] }}"
+                         class="object-cover shadow-none lg:shadow-xl rounded-xl
                                         ease-in-out duration-300
                                         lg:border-none border border-primary
                                         w-full
                                         max-h-100
                                         "
-                                 :class="{
-                                    'lg:translate-y-4 lg:-translate-x-4': hovering,
-                                    'scale-5 transition': hovering,
-                                 }"
-                                 alt=""
-                                 {!! $item['image_properties'] !!}
-                            >
-                        </div>
-                    </div>
+                         alt=""
+                            {!! $item['image_properties'] !!}
+                    >
+                </div>
+            </div>
 
 
-                    <div @class([
+            <div @class([
                         'text-center',
                         'max-w-md sm:max-w-2xl ',
                         'lg:px-0 lg:py-20',
                         'mx-auto lg:mx-0',
                         'lg:text-left' => $flip,
                         'lg:text-right' => !$flip,
-                    ])>
-                        <h2 class="text-2xl md:text-5xl underline text-primary">
-                            {{ $item['name'] }}
-                        </h2>
-                        <div class="mb-10 text-lg md:text-3xl">
-                            {{ str($item['position'])->explode('/')->join(' | ') }}
-                        </div>
-                        <div class="text-md md:text-2xl">
-                            {{ $item['blurb'] }}
-                        </div>
-                    </div>
+                    ])
 
+                 data-aos="{{ $flip ? 'fade-left' : 'fade-right' }}"
+            >
+                <h2 class="text-2xl md:text-5xl underline text-primary">
+                    {{ $item['name'] }}
+                </h2>
+                <div class="mb-10 text-lg md:text-3xl">
+                    {{ str($item['position'])->explode('/')->join(' | ') }}
+                </div>
+                <div class="text-md md:text-2xl">
+                    {{ $item['blurb'] }}
                 </div>
             </div>
+
         </div>
+
 
     @endforeach
 
