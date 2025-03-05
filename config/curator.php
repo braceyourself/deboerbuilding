03229d053143f46c571f226d3dc1f5639dacc662
Ethan Brace <ethanabrace@gmail.com>
2025-03-05 15:47:09 -0500

    test updates to uploading

--- config/curator.php
+++ config/curator.php
@@ -40,7 +40,7 @@ return [
     'is_limited_to_directory' => false,
     'is_tenant_aware' => true,
     'tenant_ownership_relationship_name' => 'tenant',
-    'max_size' => 5000,
+    'max_size' => 10000,
     'model' => Media::class,
     'min_size' => 0,
     'path_generator' => null,
