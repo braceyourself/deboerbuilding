9b4b48585b648a1e7ae908b277a00324e70b2233
braceyourself <ethanabrace@gmail.com>
2025-03-15 13:07:04 -0400

    optimize more

--- resources/views/livewire/testimonials-page.blade.php
+++ resources/views/livewire/testimonials-page.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-secondary px-10">
     <section id="testimonials" class="mx-auto py-10 bg-secondary">
-        <h2 class="text-3xl md:text-5xl pb-10 font-bold text-center">
+        <h2 class="max-[366px]:text-3xl min-[366px]:text-4xl md:text-5xl pb-10 font-bold text-center">
             Customer <span class="text-primary-500 underline">Testimonials</span>
         </h2>
         <div class="container mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 font-bold text-lg m-auto  max-w-7xl">
