630ffc3871745c694217285c36dc6731090abd16
braceyourself <ethanabrace@gmail.com>
2025-06-05 13:51:33 -0400

    fix email address

--- resources/views/components/landing/contact.blade.php
+++ resources/views/components/landing/contact.blade.php
@@ -2,7 +2,7 @@
     <h2 class="text-3xl font-bold">Contact</h2>
 
     <p class="mt-4">Phone: <a href="tel: +6163633277" target="_blank">(616) 363-3277</a></p>
-    <p>Email: info@deboerbuilding.com</p>
+    <p>Email: deboerbuilding@gmail.com</p>
     <p>Hours: M-F 8am-5pm </p>
     <p class="ml-0 md:ml-10">Office closed on Fridays, but crew works M-F</p>
 {{--    <a href=""--}}
