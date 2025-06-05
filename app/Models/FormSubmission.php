0266099f85caf4b38f75bd53c90b94240447a69c
braceyourself <ethanabrace@gmail.com>
2025-06-05 09:09:09 -0400

    change timezone and date format

--- app/Models/FormSubmission.php
+++ app/Models/FormSubmission.php
@@ -6,6 +6,11 @@ use Carbon\Carbon;
 use App\Http\Clients\N8N\N8N;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $form
+ * @property array $data
+ * @property Carbon $created_at
+ */
 class FormSubmission extends Model
 {
     protected $casts = [
@@ -15,9 +20,9 @@ class FormSubmission extends Model
     protected static function booted()
     {
         static::created(function (FormSubmission $submission) {
-            $res = N8N::post('form-submission', [
+            N8N::post('form-submission', [
                 $submission->form .' Form Submission' => $submission->data,
-                'Received At' => $submission->created_at->format(Carbon::RFC850),
+                'Received At' => $submission->created_at->timezone('America/Detroit')->format('l, M d Y h:ia'),
             ]);
         });
     }
